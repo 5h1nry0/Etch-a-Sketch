2349a931cc8b4a228d63cb6c3671ebba23b60e2f
Shinryo <157136484+5h1nry0@users.noreply.github.com>
2024-02-07 14:56:39 -0500

    Add button to let user input number of squares Also the click event of the button deletes previous grid.

--- index.js
+++ index.js
@@ -8,21 +8,46 @@
 //}
 //createGrid() 
 
-function createGrid() {
-    const container = document.querySelector("#container"); 
-    for (let i = 0; i < 256; i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
-    }
+//function createGrid() {
+//    const container = document.querySelector("#container"); 
+//    for (let i = 0; i < 256; i++) {
+//        let square = document.createElement("div");
+//        square.classList.add("square");
+//        container.appendChild(square);
+//    }
 
+//}
+
+//createGrid() 
+
+//let hoverEffect = document.getElementsByClassName("square")
+//for (let i=0; i<hoverEffect.length; i++) {
+//    hoverEffect[i].addEventListener('mouseover',() => {
+//        hoverEffect[i].classList.add("hover");
+//    })
+//}
+
+const container = document.querySelector('#container');
+removeAllChildNodes(container);
+
+let button = document.querySelector('button')
+button.addEventListener('click',() => {
+let number = prompt('Type here the number of squares per side')
+removeAllChildNodes(container);
+for (let i = 0; i < number*number; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
 }
-
-createGrid() 
-
 let hoverEffect = document.getElementsByClassName("square")
 for (let i=0; i<hoverEffect.length; i++) {
     hoverEffect[i].addEventListener('mouseover',() => {
         hoverEffect[i].classList.add("hover");
     })
+}
+}) 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
